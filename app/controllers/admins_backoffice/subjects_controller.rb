eb3d7ceab5c5ad2d29514a7438a11900a09e9a48
felipe almeida <felipe_nunesrodrigues@hotmail.com>
2021-09-01 12:26:05 +0000

    Criando as views (new e edit) para os Assuntos_Áreas

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -2,17 +2,17 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   before_action :set_subject, only: [:edit, :update, :destroy]
 
   def index
-    @subjects =Subject.all.page(params[:page])
+    @subjects = Subject.all.order(:description).page(params[:page])
   end
 
   def new
-    @subject =Subject.new
+    @subject = Subject.new
   end
 
   def create
-      @subject =Subject.new(params_subject)
-      if @subject.save
-      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Area cadastrado com sucesso!"
+    @subject = Subject.new(params_subject)
+    if  @subject.save
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso!"
     else
       render :new
     end
@@ -21,30 +21,29 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   def edit
   end
 
-  def update
-    
-    if @subject.update(params_subject)
-      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Area atualizado com sucesso!"
+  def update    
+    if  @subject.update(params_subject)
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"
     else
       render :edit
     end
   end
 
   def destroy
-      if @subject.destroy
-      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Area excluído com sucesso!"
+    if  @subject.destroy
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área excluído com sucesso!"
     else
       render :index
     end
   end
 
   private
-
-  def params_admin
+  
+  def params_subject
     params.require(:subject).permit(:description)
   end
 
   def set_subject
-    @subject = Admin.find(params[:id])
+    @subject = Subject.find(params[:id])
   end
 end
