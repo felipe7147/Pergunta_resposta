6aee79f5b7c64c4c309b3b9bbb3a2fc8a4d1cc7e
felipe almeida <felipe_nunesrodrigues@hotmail.com>
2021-09-13 21:13:01 +0000

    Adicionando a barra de pesquisa

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
+    get 'search', to: 'search#questions'
   end
   namespace :users_backoffice do
     get 'welcome/index'
+
   end
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
